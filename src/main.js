42487510d86c8a3e914a32c1fbb117e53ad12e17
罗琦 <401997126@qq.com>
2018-12-21 20:29:16 +0800

    直接引用现成工程，修改mock登录接口，完成第一个egg的登录接口对接

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import Element from 'element-ui';
+import scroll from 'vue-seamless-scroll';
+import Icon from 'vue-svg-icon/Icon.vue';
+import '@/theme/element-#41B883/index.css';
+import '@/assets/styl/index';
+import '@/mock';
 
-Vue.config.productionTip = false
+import App from '@/App';
+import { router } from '@/router';
+import '@/router/routerControl';
+import store from '@/store';
+
+import i18n from '@/lang';
+
+Vue.component('icon', Icon);
+
+Vue.use(scroll);
+Vue.use(Element, {
+  size: 'medium',
+  i18n: (key, value) => i18n.t(key, value)
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
+  el: '#app',
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  i18n,
+  components: { App },
+  template: '<App/>'
+});
